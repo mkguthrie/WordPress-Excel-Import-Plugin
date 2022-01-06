72614f04d0fdd97c4065d100050c6d03d0147530
Matt <matthew.k.guthrie@gmail.com>
2022-01-06 07:50:44 -0700

    plugin activation file updates

--- import-and-datatables.php
+++ import-and-datatables.php
@@ -19,4 +19,33 @@ if (!class_exists('name_here')) :
         }
     }
 
+    function name_here() {
+        global $plugin_data;
+        if(!isset($plugin_data)) {
+            $plugin_data = new name_here();
+            $plugin_data->init();
+        }
+    }
+
+    function name_here_active() {
+        global $wpdb;
+        $charset_collate = $wpdb->get_charset_collate();
+        $tablename = $wpdb->prefix . "table_name_here";
+        $sql = "CREATE TABLE $tablename (
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            manufacturer varchar(255) NOT NULL,
+            part_number varchar(255) NOT NULL,
+            part_description text character set utf8 NOT NULL,
+            quantity_available varchar(255) NOT NULL,
+            price_quantities varchar(255) NOT NULL,
+            price_usd varchar(255) NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+        dbDelta($sql);
+    }
+
+    register_activation_hook(__FILE__, 'name_here_activate');
+
+    name_here();
+
 endif;
