95e89041b4e541b9e30fda7d77df228dd1a636b8
Matt <matthew.k.guthrie@gmail.com>
2022-01-06 07:39:57 -0700

    add main plugin file

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a plugin that imports an .xls file into a custom table of the WordPress database. There is also a shortcode to show a table on the front end of the site.
---
+++ import-and-datatables.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Excel Import and Datatables
+ * Plugin URI:
+ * Description: Pulls in a excel file and creates a list of the data.
+ * Version: 1.0.0
+ * Author: Matt Guthrie
+ * Author URI: http://www.mattguthrie.co
+ * License: GPL2
+ */
+
+define('EXCEL_IMPORT_AND_DATATABLES', '1.0.0');
+
+if (!class_exists('name_here')) :
+    class name_here {
+        function init() {
+            //includes
+            // include_one('includes/');
+        }
+    }
+
+endif;
